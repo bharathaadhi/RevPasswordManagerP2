374d3fe6112418edee3d1e3df1bbe205797ca96f
aadhi <bharathaadhi32@gmail.com>
2026-02-18 12:14:40 +0530

    Added UserRepository and implemented AuditController with security report, weak and reused password endpoints

---
+++ src/main/java/com/rev/revpasswordmanagerp2/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.rev.revpasswordmanagerp2.repository;
+
+import com.rev.revpasswordmanagerp2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
